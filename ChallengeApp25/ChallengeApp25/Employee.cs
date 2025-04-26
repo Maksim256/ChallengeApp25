c251ab93b07578085aade2ba58162113f34dd092
Maksim256 <andrzej.o@windowslive.com>
2025-04-26 10:46:29 +0200

    zadanie domowe 6

---
+++ ChallengeApp25/ChallengeApp25/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp25
+{
+    public class Employee
+    {
+        private string imie { get; set; }
+        private string nazwisko { get; set; }
+        private int wiek { get; set; }
+
+        private List<int> punkty = new List<int>();
+        public Employee()
+        {
+            this.imie = "-";
+            this.nazwisko = "-";
+            this.wiek = 0;
+        }
+        public Employee(string imie, string nazwisko, int wiek )
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.wiek = wiek;
+        }
+        public Employee(string imie)
+        {
+            this.imie = imie;
+            
+        }
+        public string Imie
+        {
+            get { return this.imie; }
+            set { this.imie = value; }
+        }
+        public string Nazwisko
+        {
+            get { return this.nazwisko; }
+            set { this.nazwisko = value; }
+        }
+        public int Wiek
+        {
+            get { return this.wiek; }
+            set { this.wiek = value; }
+        }
+
+        public int Punkty
+        {
+            get
+            {
+                return this.punkty.Sum();
+            }
+        }
+        public void DodajPunkty(int punkty)
+        {
+            
+            this.punkty.Add(punkty);
+        }
+
+    }
+}
--- ChallengeApp25/ChallengeApp25/Program.cs
+++ ChallengeApp25/ChallengeApp25/Program.cs
@@ -6,6 +6,13 @@ User user2 = new User("Monika","88995588");
 User user3 = new User("Zuzia","99556688");
 User user4 = new User("Damian","4455887755");
 
+Employee employee1 = new Employee("aaa","nazwis1",23);
+Employee employee2 = new Employee("bbbb","nazwisko2", 28);
+Employee employee3 = new Employee("cccc","nazwisko3", 38);
+Employee employee4 = new Employee("ddd","nazwisko4", 50);
+Employee employee5 = new Employee("eee","nazwisko5", 55);
+
+
 //user1.Login = "Marek";
 var name =user1.Login;
 user1.AddScore(5);
@@ -16,4 +23,47 @@ User.GameName ="Doom";
 var pi =  Math.PI;
 Console.WriteLine(pi);
 
+//var imie = employee1;
+//Console.WriteLine(employee1.Punkty);
+employee1.DodajPunkty(1);
+employee1.DodajPunkty(2);
+employee1.DodajPunkty(55);
 
+employee2.DodajPunkty(2);
+employee2.DodajPunkty(3);
+employee2.DodajPunkty(24);
+
+employee3.DodajPunkty(5);
+employee3.DodajPunkty(6);
+employee3.DodajPunkty(7);
+
+employee4.DodajPunkty(8);
+employee4.DodajPunkty(9);
+employee4.DodajPunkty(10);
+employee5.DodajPunkty(41);
+employee5.DodajPunkty(12);
+employee5.DodajPunkty(13);
+employee5.DodajPunkty(14);
+
+
+
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1,employee2,employee3,employee4,employee5
+};
+
+int maxPoints = -1;
+
+Employee topEmployees = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Punkty > maxPoints)
+    {
+        topEmployees = employee;
+        maxPoints = employee.Punkty;
+    }
+    
+}
+Console.WriteLine("najlepszy" +" "+ topEmployees.Nazwisko, "punkty " );
