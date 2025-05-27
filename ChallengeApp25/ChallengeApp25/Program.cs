292a9c0850b1c22566132e2e926501b46bc73615
Maksim256 <andrzej.o@windowslive.com>
2025-05-27 08:38:44 +0200

    dz9 koniec, przed usunieciem usera i testow

--- ChallengeApp25/ChallengeApp25/Program.cs
+++ ChallengeApp25/ChallengeApp25/Program.cs
@@ -4,8 +4,16 @@ using ChallengeApp25;
 var employee = new Employee("Adam", "Nowak");
 employee.AddGrade(3);
 employee.AddGrade(4);
+employee.AddGrade(3);
 var statistics = employee.GetStatistics();
 
-Console.WriteLine($"Max: {statistics.Max}"); 
+Console.WriteLine($"Max: {statistics.Max} Min: {statistics.Min} Average: {statistics.Average:N2}"); 
+SetSth(out statistics);
 
-
+void SetSth(out Statistics statistics)
+{
+    // This method is intentionally left blank.
+    // It serves as a placeholder for future functionality or testing purposes.
+    // Currently, it does not perform any operations or return any values.
+    statistics = new Statistics();
+}
